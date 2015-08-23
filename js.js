4fb3bd9723ce8c93cac97938a2e1ec60d2349496
Sebastien Powell <sebastienpowell@gmail.com>
2015-08-23 21:18:09 +0100

    Finished first working version

--- js.js
+++ js.js
@@ -1,54 +1,46 @@
-(function($) {
-    $.fn.modal = function(options) {
-  			var openModals = [];
+(function($) { 
+  $.fn.modal = function(options) {
+    var closeModal, settings;
+    settings = $.extend({
+      targetElement: ".modal",
+      modalShowClass: 'is-open',
+      modalBackdropClass: '.modal-backdrop'
+    }, options);
 
-  			var settings = $.extend({
-  		    modalShowClass : "show",
-          modalCloseClass : "show",
-  		    transitionSpeed: "0.25s",
-  		    transition: "fadeIn"
-  			}, options);
+    // Center modals on screen. 
+    this.each(function(index, element) {
+      var modalWidth;
+      modalWidth = $(element).outerWidth() / 2;
+      $(this).css('margin-left', -modalWidth);
+    });
 
-  			// Center modals on screen. 
-  			this.each(function(index, element) {
-  			   var modalWidth = ($(element).outerWidth()) / 2;
-  			  $(this).css("margin-left", -modalWidth);
-  			});
+    // Show target modal & add backdrop.
+    $('[data-modal]').on('click', function(e) {
+      var targetModal, targetModalID;
+      e.preventDefault();
+      e.stopPropagation();
+      targetModalID = $(this).data('modal');
 
+      targetModal = $(settings.targetElement).filter(function() {
+        return $(this).attr('id') === targetModalID;
+      });
 
+      targetModal.addClass(settings.modalShowClass);
+      $(settings.modalBackdropClass).addClass(settings.modalShowClass);
+      $('body').css('overflow', 'hidden');
+    });
 
-  			$("[data-modal]").on('click', function(e) {
-          
-  				e.preventDefault();
-  				var targetModalID = $(this).data("modal");
-  				var targetModal = $(".modal").filter(function() { 
-                              return $(this).attr("id") == targetModalID
-                            });
+    function closeModal() {
+      $(settings.targetElement).add(settings.modalBackdropClass).removeClass(settings.modalShowClass);
+      $('body').css('overflow', 'hidden');
+    };
 
-  				
-          // Show target modal & add backdrop
-  				targetModal.addClass(settings.modalShowClass);
-  				$(".modal-backdrop").addClass("show");
-          $("body").css("overflow", "hidden");
-  			});
+    this.find('[data-modal = \'modal-close\']').click(function() {
+      closeModal();
+    });
 
-  			// Close active modal. 
-  			this.find("[data-modal = 'modal-close']").click(function() {
-          $(this).parents(".modal").removeClass(settings.modalShowClass);
-          $(".modal-backdrop").removeClass("show");
-          $("body").css("overflow", "hidden");
-  			});
-
-        // ("[data-modal]" + ", .modal-backdrop")
-
-  			// Add transition
-  			if (settings.transitionSpeed) {
-  				this.each(function() {
-  					$(this).css({"transition": "opacity" + " " + settings.transitionSpeed,});
-  				});
-  			}
-  		}
-}(jQuery));
-
-
-
+    return $(".modal-backdrop").on('click', function(e) {
+      closeModal();
+    });
+  };
+})(jQuery);
